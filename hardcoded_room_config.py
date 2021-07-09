00661e9013f7577e633bf6e28c499a6ef0615452
Alex Morgan <lexyym@gmail.com>
2021-07-09 12:33:17 -0400

    adjust order of levels in Westin

--- hardcoded_room_config.py
+++ hardcoded_room_config.py
@@ -3,6 +3,11 @@ from location import *
 # For now we are hard-coding the levels/rooms/etc.
 # TO-DO: Definitely read in from config file before production!!!
 w3 = Level("Mezzanine", "3W")
+w2 = Level("Lobby West", "2W")
+w1 = Level("Concourse Level", "1W")
+conf = Level("Conference Level", "3E")
+e2 = Level("Lobby East", "2E")
+
 w3.add_room('Alcott')
 w3.add_room('Adams')
 w3.add_room('Board Room')
@@ -12,7 +17,6 @@ w3.add_room('Faneuil')
 w3.add_room('Hale')
 w3.add_room('Pool')
 
-conf = Level("Conference Level", "3E")
 conf.add_room('Burroughs')
 conf.add_room('Griffin')
 conf.add_room('Independence')
@@ -22,7 +26,6 @@ conf.add_room('Harbor Prefunction')
 conf.add_room('Harbor Ballroom I', 'Harbor I').suppress()
 h2 = conf.add_room('Harbor Ballroom II-III', 'Harbor II-III')
 
-w2 = Level("Lobby West", "2W")
 w2.add_room("Otis")
 w2.add_room("Paine")
 w2.add_room("Quincy")
@@ -36,13 +39,11 @@ webster.add_section("Webster-FastTrack3")
 webster.add_section("Webster-FastTrack4")
 w2.add_room("Lobby")
 
-e2 = Level("Lobby East", "2E")
 e2.add_room("Marina 1")
 e2.add_room("Marina 2")
 e2.add_room("Marina 3")
 e2.add_room("Marina 4")
 
-w1 = Level("Concourse Level", "1W")
 w1.add_room("Grand Prefunction")
 a = w1.add_room("Grand Ballroom A", "Grand A")
 b = w1.add_room("Grand Ballroom B", "Grand B")

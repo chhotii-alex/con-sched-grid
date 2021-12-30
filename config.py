b414cf3a46e56ff542d9252b6a49ec78feabaa38
Alex Morgan <lexyym@gmail.com>
2021-12-30 08:25:29 -0500

    handle aliases (for gaming tables)

--- config.py
+++ config.py
@@ -10,6 +10,16 @@ cfg.read("example-data/conguide.cfg")
 
 event_name = cfg.get('convention', 'convention')
 
+# Notice the differences between sections, grid rooms, and aliases:
+# If grid rooms is specified, each grid room is labeled with its own
+# name on its own row and when not merged, there is a border.
+# For example Grand A + Grand B = Grand AB.
+# If sections is specified, only one label is used, there is no border
+# seperating the sections on the grid, but the row is double height
+# (really two rows so each can have independent programming).
+# For example Hancock consists of Hancock-FastTrack1 and Hancock-FastTrack2.
+# If aliases are specified, they are really just aliases. One row is 
+# generated with one label and everything goes into that row.
 for section in cfg.sections():
     m = re.match(r'room (.*)', section)
     if m:
@@ -36,6 +46,13 @@ for section in cfg.sections():
             room = location.Room(name, usage, pubsname)
             for c in corners:
                 room.add_section(c)
+        try:
+            aliases = cfg.get(section, 'aliases')
+            aliases = re.split(r',\s*', aliases)
+            for al in aliases:
+                location.gLocationLookup[al] = location.gLocationLookup[name]
+        except configparser.NoOptionError:
+            pass
 
 for section in cfg.sections():
     m = re.match(r'(level|venue) (.*)', section)
@@ -51,6 +68,13 @@ for section in cfg.sections():
             if rname not in location.gLocationLookup:
                 location.Room(rname)
             level.add_room(location.gLocationLookup[rname])
+
+# Use aliases on the room definition, because
+# THIS DOES NOT WORK, because the case is smashed for the key:
+#for mapping in cfg.items("session change room"):
+#    location.gLocationLookup[mapping[0]] = location.gLocationLookup[mapping[1]]
+
+print(location.gLocationLookup)
 
 session_initial_abbreviations = {}
 session_continue_abbreviations = {}

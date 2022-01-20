99fc3df7c982e6ba4c8844a4b0292c7aab8ce196
Alex Morgan <lexyym@gmail.com>
2022-01-20 05:57:36 -0500

    maybe wanted for conguide HTML

--- cleaner.py
+++ cleaner.py
@@ -11,6 +11,59 @@ def replace_all(text, dic):
         text = text.replace(i, j)
     return text
 
+def tag_parts(tag_str):
+    parts = tagparts.fullmatch(tag_str)
+    is_close = parts.group(1) == '/'
+    attrs = parts.group(3)
+    is_empty = not is_close and len(attrs) > 0  and attrs[-1] == '/'
+    if is_empty:
+        attrs = attrs[:-1]
+    tag = parts.group(2)
+    return (tag, attrs, is_close, is_empty)
+
+'''
+Note that this is severely buggy if an attribute
+value contains whitespace. However, I cannot think of any
+legitimate reason that an <a> tag would have an attribute
+with a value containing whitespace in this context.
+'''
+def cleanup_anchor_attributes(attrs):
+    attrs = attrs.strip()
+    attrs = attrs.split()
+    dict = {}
+    for attr in attrs:
+        keyval = attr.split("=", 1)
+        if len(keyval) == 1:
+            dict[attr] = None
+        else:
+            dict[keyval[0]] = keyval[1]
+    should_target_blank = False
+    if 'href' in dict:
+        href_val = dict['href'].strip('"')
+        if not href_val.startswith('#'):
+            should_target_blank = True
+    if should_target_blank:
+        dict['target'] = '"_blank"'
+    result = ''
+    for key in dict:
+        result += " "
+        if dict[key] is None:
+            result += key
+        else:
+            result += "%s=%s" % (key, dict[key])
+    return result
+
+def cleanup_tag(tag, attrs, is_close, is_empty):
+    close_mark = ''
+    if is_close:
+        close_mark = '/'
+    empty_mark = ''
+    if is_empty:
+        empty_mark = '/'
+    if tag == 'a':
+        attrs = cleanup_anchor_attributes(attrs)
+    return "<%s%s%s%s>" % (close_mark, tag, attrs, empty_mark)
+
 '''
 Return values: transformed string; whether original string contained forbidden tag
 Throws exception if orginal string contains unmatched tags
@@ -24,15 +77,9 @@ def clean_tags(s, allowed_tags):
         bit = pieces[i]
         is_tag = i%2
         if is_tag:
-            parts = tagparts.fullmatch(bit)
-            is_close = parts.group(1) == '/'
-            tag = parts.group(2)
-            attrs = parts.group(3)
-            is_empty = len(attrs) > 0  and attrs[-1] == '/'
-            if is_empty:
-                attrs = attrs[:-1]
+            (tag, attrs, is_close, is_empty) = tag_parts(bit)
             if tag in allowed_tags:
-                result += bit
+                result += cleanup_tag(tag, attrs, is_close, is_empty)
             else:
                 result += " "
                 contains_forbidden_tag = True
@@ -84,8 +131,23 @@ def test_result_is_empty(s):
     if new_str:
         raise Exception("Result should be empty")
 
+def test_anchor_cleaning(s):
+    (new_str, forbidden_tag) = clean_tags(s, ['i', 'a'])
+    if 'target="_blank"' not in new_str:
+        raise Exception("Failed to add target=_blank")
+
+def test_internal_anchor(s):
+    (new_str, forbidden_tag) = clean_tags(s, ['i', 'a'])
+    if 'target="_blank"' in new_str:
+        raise Exception("Should not add target=_blank to internal anchors")
+
 def run_unit_tests():
+    test_anchor_cleaning('this text <a href="https://www.example.com">contains a yummy link</a>okay?')
+    test_internal_anchor('trying <a href="#top">internal anchor link</a>')
+    test_internal_anchor('this makes <a id="yo"/>an anchor on the page')
     test_string_is_unchanged('before<i>Lord of the Rings</i>after')
+    test_string_is_unchanged('testing an empty <i/> tag')
+    test_string_is_unchanged('not that an <i floppy="true">italic</i>can have attributes')
     test_contains_and_doesnt('blah<meta charset="utf-8" />5 < 8', '&lt;', 'meta')
     test_contains_and_doesnt('before<meta name="viewport" content="width=device-width, initial-scale=1.0" />after', 'after', 'viewport')
     test_contains_and_doesnt('before<link rel="canonical" href="https://docs.python.org/3/library/re.html" />after', 'after', 'https')
